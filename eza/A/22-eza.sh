802e82e6dd1444bff34edfa6820eeb685e72155d
Toxikuu <toxikuu@users.noreply.github.com>
2025-06-07 11:55:17 -0500

    pkg(bump): eza 0.21.3 -> 0.21.4
    Tweaks, bump

--- eza/A/22-eza.sh
+++ eza/A/22-eza.sh
@@ -3,6 +3,6 @@
 # Set eza as an ls replacement
 if command -v eza > /dev/null 2>&1; then
     alias ls="eza --icons=always --color=always --group-directories-first -F=always --header --smart-group --mounts --octal-permissions --git --no-quotes"
-    alias l="ls -l --total-size" # this can be very slow depending where it's run
     alias lt="ls -T"
+    alias lT="ls -l --total-size" # this can be very slow depending where it's run
 fi
